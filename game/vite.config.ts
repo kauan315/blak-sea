23bcfdd584c09e0384fe71e75fe79abb1b51b878
kauan315 <kauankassiano335@gmail.com>
2026-09-14 12:59:21 -0500

    Configure Vite game entry

---
+++ game/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [{
+    name: 'tidebreakers-entry',
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [{ tag: 'script', attrs: { type: 'module', src: '/src/main.ts' }, injectTo: 'body' }]
+      };
+    }
+  }]
+});
