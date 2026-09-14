f4ee66b3cdc55794259036de3439a98fc2324377
kauan315 <kauankassiano335@gmail.com>
2026-09-14 13:02:58 -0500

    Remove duplicate Vite entry injection

--- game/vite.config.ts
+++ game/vite.config.ts
@@ -1,11 +1,3 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [{
-    name: 'tidebreakers-entry',
-    transformIndexHtml(html) {
-      const tag = '<scr' + 'ipt type=\"module\" src=\"/src/main.ts\"></scr' + 'ipt>';
-      return html.replace('</body>', tag + '</body>');
-    }
-  }]
-});
+export default defineConfig({});
